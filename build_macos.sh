a23457f34af7247658a59b3fc520e60d2a45ad67
zyqyq <zyqyq@qq.com>
2025-03-13 23:17:12 +0800

    temp6

--- build_macos.sh
+++ build_macos.sh
@@ -72,7 +72,7 @@ zip -r "$WindSendRustBin_X86_64DirName".zip "$WindSendRustBin_X86_64DirName"
 
 # 新增 .app 和 .dmg 打包逻辑
 ICONS_PATH="${WINDSEND_PROJECT_PATH}/app_icon/macos/AppIcon.icns"
-ICON_PATH="$WINDSEND_RUST_PROJECT_PATH/$SERVER_PROGRAM_ICON_NAME"
+ICON_PATH="${WINDSEND_PROJECT_PATH}/$WINDSEND_RUST_PROJECT_PATH/$SERVER_PROGRAM_ICON_NAME"
 APP_NAME="Windsend"
 APP_BUNDLE="${APP_NAME}.app"
 
@@ -95,9 +95,9 @@ cat <<EOF > "${APP_BUNDLE}/Contents/Info.plist"
     <key>CFBundleName</key>
     <string>${APP_NAME}</string>
     <key>CFBundleVersion</key>
-    <string>${WINDSEND_PROJECT_VERSION}</string>
+    <string>1.4.101</string>
     <key>CFBundleShortVersionString</key>
-    <string>${WINDSEND_PROJECT_VERSION}</string>
+    <string>1.4.101</string>
     <key>CFBundleIconFile</key>
     <string>AppIcon.icns</string>
     <key>LSUIElement</key>
